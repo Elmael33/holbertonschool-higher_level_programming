efb4a88b3a118ceadd56b4b2505a7ad378bce00c
Elmael33 <mael.salet33@gmail.com>
2024-05-15 16:07:56 +0200

    [fix] 0-square_matrix_simple.py; change syntax for row_2

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,8 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        row_2 = [x**2 for x in row]
+        row_2 = []
+        for x in row:
+            row_2.append(x ** 2)
         new_matrix.append(row_2)
     return new_matrix
