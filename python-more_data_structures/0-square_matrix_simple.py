bc883c6e36ee0cf8992d8918f7d9350f8915f676
Elmael33 <mael.salet33@gmail.com>
2024-05-16 23:19:17 +0200

    [fix] 0-square_matrix_simple.py; change syntax and use list(map(lambda))

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        row_2 = []
-        for x in row:
-            row_2.append(x ** 2)
-        new_matrix.append(row_2)
+        new_row = list(map(lambda x: x ** 2, row))
+        new_matrix.append(new_row)
     return new_matrix
