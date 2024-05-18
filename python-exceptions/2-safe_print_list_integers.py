6aa0592f102a0169d888466826de95ba373291bc
Elmael33 <mael.salet33@gmail.com>
2024-05-18 18:54:56 +0200

    [fix] 2-safe_print_list_integers.py; add a variable init a 0 to count print integer in my_list and itterate 1 at each print, return num total

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,12 +2,15 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     try:
+        num = 0
         for i in range(x):
             try:
                 print("{:d}".format(my_list[i]), end="")
+                num += 1
             except (ValueError, TypeError):
                 pass
     except IndexError:
         pass
     finally:
         print()
+        return num
