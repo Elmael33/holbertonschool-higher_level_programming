7841ef45eb90c8088d2b52654659ded8851a0cc5
Elmael33 <mael.salet33@gmail.com>
2024-05-19 15:23:34 +0200

    [add] 1-square.py; code a class Square that defines a square based on 0-square.py fonction

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class named Square.
+"""
+
+
+class Square:
+    """
+    This is an empty class that represents a square.
+    """
+    def __init__(self, size):
+        self.__size = size
