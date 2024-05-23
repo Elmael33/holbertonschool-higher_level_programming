0abd82827f8f7926c1a39197af2ffec6e226bc73
Elmael33 <mael.salet33@gmail.com>
2024-05-23 16:27:34 +0200

    [add] 0-rectangle.py; function that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class named rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class that represents a rectangle.
+    """
+    pass
