2f5d05532afd0a499417253e06a362792fd74787
Elmael33 <mael.salet33@gmail.com>
2024-05-25 14:07:32 +0200

    [add] 1-rectangle.py; defines a rectangle based on 0-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class named rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class that represents a rectangle.
+    """
+    pass
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle with width and height."""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
