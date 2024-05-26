485aa7c5735a0106283f44545f4001f9c1ff15e0
Elmael33 <mael.salet33@gmail.com>
2024-05-26 18:16:40 +0200

    [add] task_00_abc.py; implemented an abstract base class Animal with subclasses

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+This module defines an abstract class Animal and its subclasses Dog and Cat.
+"""
+
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    """
+    An abstract base class representing an animal.
+    """
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    """
+    A class representing a dog, a type of animal.
+    """
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    """
+    A class representing a cat, a type of animal.
+    """
+    def sound(self):
+        return "Meow"
