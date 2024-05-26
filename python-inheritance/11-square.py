5e7f11d1035288a17fea9e168b720cb0ef5a9a3e
Elmael33 <mael.salet33@gmail.com>
2024-05-26 17:36:20 +0200

    [add] 11-square.py; inherits from 9-rectangle.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module defines the Rectangle class that inherits from BaseGeometry.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class representing a square using Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance with size.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the area of the square.
+        """
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+        """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
