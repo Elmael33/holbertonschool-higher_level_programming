f34aadeca1666ac5e7fb52be09671fd165059a06
Elmael33 <mael.salet33@gmail.com>
2024-05-29 15:34:49 +0200

    [add] 7-add_item.py; save command line arguments to a JSON file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+import os
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+if os.path.exists(filename):
+    array = load_from_json_file(filename)
+else:
+    array = []
+
+array.extend(sys.argv[1:])
+
+save_to_json_file(array, filename)
