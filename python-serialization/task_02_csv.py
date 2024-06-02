a3ae65f929dc9adbbfd2743ab1a06eeed466b41f
Elmael33 <mael.salet33@gmail.com>
2024-06-02 18:21:00 +0200

    [add] task_02_csv.py; reading data from one format CSV and serialize it using JSON

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains the convert_csv_to_json function
+"""
+import csv
+import json
+
+
+def convert_csv_to_json(csv_file):
+    """
+    Converts data from a CSV file to JSON format and writes it to data.json.
+    """
+    my_list = []
+    try:
+        with open(csv_file, "r", encoding="utf-8") as myFile:
+            csvReader = csv.DictReader(myFile)
+            for rows in csvReader:
+                my_list.append(rows)
+
+        with open("data.json", "w", encoding="utf-8") as myFile:
+            json.dump(my_list, myFile, indent=1)
+
+        return True
+    except (
+            FileNotFoundError, AttributeError,
+            EOFError, ImportError, IndexError
+            ):
+        return False
