3b84177f0d915c9b19711c22fa21925cd5c536bf
Elmael33 <mael.salet33@gmail.com>
2024-06-20 10:36:02 +0200

    [add] 1-creat_user.sql; script that creates the MySQL server user user_0d_1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Create MySQL user 'user_0d_1' if it doesn't already exist and grant all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
