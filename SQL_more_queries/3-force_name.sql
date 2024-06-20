6f0338e32c23b4253e84c50ea2803692a3bbd651
Elmael33 <mael.salet33@gmail.com>
2024-06-20 22:24:11 +0200

    [add] 4-never_empty.sql; script that creates the table id_not_null on your MySQL server

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
--- Create table force_name if it doesn't already exist
+-- Create table force_name if it doesn't exist
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
