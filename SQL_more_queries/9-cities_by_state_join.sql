7727306bddd487df840cf8218fbc739e37bbe3db
Elmael33 <mael.salet33@gmail.com>
2024-06-21 19:25:01 +0200

    [fix] 9-cities_by_state_join.sql; syntax mysql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
 -- Lists all cities with their corresponding state names
-SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
-FROM cities
-ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name
+    FROM cities
+       INNER JOIN states
+       ON cities.state_id = states.id
+ORDER BY cities.id ASC;
