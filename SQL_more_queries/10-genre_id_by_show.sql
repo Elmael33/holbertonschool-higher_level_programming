58ecdfbd3d5d1c36a2e22d49318825105362094c
Elmael33 <mael.salet33@gmail.com>
2024-06-21 21:20:24 +0200

    [fix] 10-genre_id_by_show.sql / hbtn_0d_tvshows.sql; add USE to hbtn and implement the list all shows contained

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,7 @@
--- List all shows with at least one genre linked
+USE hbtn_0d_tvshows;
 
+-- List all shows with at least one genre linked
+SELECT s.title, g.genre_id
+FROM tv_shows s
+INNER JOIN tv_show_genres g ON s.id = g.show_id
+ORDER BY s.title ASC, g.genre_id ASC;
