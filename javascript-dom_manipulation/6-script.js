957a5d20045f9173b46342d54d6bf00848d505a9
Elmael33 <mael.salet33@gmail.com>
2024-07-26 15:59:40 +0200

    [add] 6-script.js; implment a script that fetches the character name from a given URL

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const characterElement = document.getElementById('character');
+      characterElement.textContent = data.name;
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
