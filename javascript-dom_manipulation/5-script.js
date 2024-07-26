9e12ec47bda966c0448052b2bed067c7416daf1b
Elmael33 <mael.salet33@gmail.com>
2024-07-26 15:22:57 +0200

    [add] 5-script.js; implement a script that updates the text of the header when you click on it

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const updateHeaderElement = document.getElementById('update_header');
+  updateHeaderElement.addEventListener('click', () => {
+    const headerElement = document.querySelector('header');
+    headerElement.textContent = 'New Header!!!';
+  });
+});
